34abd401984240d86771bea5d8c067e1914624d8
NaveengrXworkz <naveengr.xworkz@gmail.com>
2021-12-20 18:14:02 +0530

    byte programme

---
+++ Byte.java
@@ -0,0 +1,15 @@
+class Byte{
+     static byte []rollno = {1,8,5,6,0,3,3,2,4,7};
+     public static void main(String[] args){
+     System.out.println(rollno[0]);
+     System.out.println(rollno[1]);
+     System.out.println(rollno[2]); 
+     System.out.println(rollno[3]);
+     System.out.println(rollno[4]);
+     System.out.println(rollno[5]);
+     System.out.println(rollno[6]);
+     System.out.println(rollno[7]);
+     System.out.println(rollno[8]);
+     System.out.println(rollno[9]);
+     }
+}
